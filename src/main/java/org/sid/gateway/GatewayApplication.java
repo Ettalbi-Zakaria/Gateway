993da649953db8ab19a74baae4631fde7d0a43d4
Ettalbi-Zakaria <z.ettalbi@etu.enset-media.ac.ma>
2023-10-28 20:23:47 +0100

    RouteLocator Configuration

--- src/main/java/org/sid/gateway/GatewayApplication.java
+++ src/main/java/org/sid/gateway/GatewayApplication.java
@@ -13,11 +13,12 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+
     @Bean
     RouteLocator routeLocator(RouteLocatorBuilder builder){
         return builder.routes()
-                .route("r1",r -> r.path("/customers/**").uri("http://localhost:8081/"))
-                .route("r2",r -> r.path("/products/**").uri("http://localhost:8082/"))
+                .route("r1",r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
+                .route("r2",r -> r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
                 .build();
     }
 }
